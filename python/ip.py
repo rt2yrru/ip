520aee6d1d3f08b02fbe22e76e7e5c284b0d009c
siddht4 <dhar.75.ap@gmail.com>
2019-09-08 14:12:10 +0530

    Update ip.py

--- python/ip.py
+++ python/ip.py
@@ -3,7 +3,4 @@ _url='http://api.ipstack.com/check?access_key=b1a25591364acdaffc213fc8c29fa202&f
 p=''
 with urllib.request.urlopen(_url) as response:
    #print(json.loads(response))
-   p=response.read()
-# for i in p:
-# 	print(i)
-print(type(p))
+   print(response)
